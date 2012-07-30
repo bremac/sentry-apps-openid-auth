908f999ebd4861a8194736909f2d2ae71051edff
Brendan MacDonell <brendan@macdonell.net>
2012-07-30 01:49:19 -0700

    Bump version, and depend on a tested version of sentry.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='sentry-apps-openid-auth',
-      version='1.0',
+      version='1.0.1',
       description='Hack to add single-domain Google Apps auth to sentry',
       author='Brendan MacDonell',
       author_email='brendan@macdonell.net',
@@ -10,7 +10,7 @@ setup(name='sentry-apps-openid-auth',
       install_requires=[
           'python-openid==2.2.5-apps',
           'django-openid-auth==0.4',
-          'sentry==4.8.2',
+          'sentry==4.8.1',
       ],
       dependency_links=[
           'git+https://github.com/bremac/python-openid.git@8744683137441cbaefecf6de0015242017df0ba1#egg=python-openid-2.2.5-apps',
